048cf92d0eae55dbb966264539a24f97ee88d105
cezary <cezarykretkowski@gmail.com>
2024-04-04 19:16:01 +0200

    Initial commit

---
+++ src/main/java/com/example/ProjectIUI_HealthBOOT/ProjectIuiHealthBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectIUI_HealthBOOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectIuiHealthBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectIuiHealthBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProjectIUI_HealthBOOT/ProjectIuiHealthBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectIUI_HealthBOOT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectIuiHealthBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
